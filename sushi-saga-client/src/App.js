387dacf75d08b875c5af30732c8101d49ab88cd0
github username <github email address>
2019-03-19 12:26:34 -0400

    something wrong with map in sushiContainer

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,11 +6,60 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    plates: [0, 1, 2, 3],
+    customerAccount: 100,
+    emptyPlates: [],
+    color: "black"
+  }
+
+
+  incrementPlatesCount = () => {
+    let newPlates = [...this.state.plates]
+    newPlates = newPlates.map(num => num + 4)
+    this.setState = {
+      plates: newPlates
+    }
+  }
+
+  addEmptyPlates = (plate) => {
+      let newEmptyPlate = [...this.state.emptyPlates, plate]
+      this.setState({ emptyPlates: newEmptyPlate })
+  }
+
+  decrementCustomerAccount = (amount) => {
+    this.setState({
+      customerAccount: this.state.customerAccount - amount
+    })
+  }
+
+  changeColor = () => {
+    this.setState({
+      color: "red"
+    })
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(res => res.json())
+    .then(allSushi => {
+      this.setState({ sushis: allSushi })
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
+
+        <SushiContainer
+          sushis={this.state.sushis}
+          display={this.state.display}
+          incrementPlatesCount={this.state.incrementPlatesCount}
+          decrementCustomerAccount={this.state.decrementCustomerAccount}
+          money={this.state.money}
+          addEmptyPlates={this.state.addEmptyPlates}
+          changeColor={this.state.changeColor} />
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
+  console.log(props)
+  const mapSushis = () =>
+    props.sushis.map(sushi => {
+      return (
+        <Sushi
+          key={sushi.id}
+          sushi={sushi}
+          decrementCustomerAccount={props.decrementCustomerAccount}
+          money={props.money}
+          incrementPlatesCount={props.incrementPlatesCount}
+          changeColor={props.changeColor}
+          />
+      )
+    })
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+          // {mapSushis()[props.plates[0]]} {mapSushis()[props.plates[1]]}{" "}}
+          // {mapSushis()[props.plates[2]]} {mapSushis()[props.plates[3]]}
+        <MoreButton incrementPlatesCount={props.incrementPlatesCount}/>
       </div>
     </Fragment>
   )
